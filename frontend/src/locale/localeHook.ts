f95c2048aebf1b73b4dc82b0dabf92d0c0619ab4
0xys <yushi.kinger@gmail.com>
2022-09-21 16:38:52 +0900

    add ja

--- frontend/src/locale/localeHook.ts
+++ frontend/src/locale/localeHook.ts
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 
 import en from './en'
 import zh from './en'
-import ja from './en'
+import ja from './ja'
 
 export const useLocale = () => {
     const { locale } = useRouter()
